be4a7d7143fd079a77da0c7ddf9289a037ee21c9
iyinraphael <iyinraphael@gmail.com>
2021-03-17 13:32:47 -0700

    readme added

---
+++ ReadME.md
@@ -0,0 +1,2 @@
+# The Name Game
+    A simple app for learning the names of WillowTree team members.
---
+++ TheNameGame/Resources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TheNameGame
+//
+//  Created by Iyin Raphael on 3/17/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
