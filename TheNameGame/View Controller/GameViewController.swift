649fc142e159a675b0a1123afc3b418c4d6adfda
iyinraphael <iyinraphael@gmail.com>
2021-03-18 10:21:46 -0700

    collectview cell added

--- TheNameGame/View Controller/GameViewController.swift
+++ TheNameGame/View Controller/GameViewController.swift
@@ -10,24 +10,13 @@ import UIKit
 class GameViewController: UIViewController {
 
     // MARK: - Properties
-    var firstImageView: UIImageView!
-    var secondImageView: UIImageView!
-    var thirdImageView: UIImageView!
-    var fourthImageView: UIImageView!
-    var fifthImageView: UIImageView!
-    var sixthImageView: UIImageView!
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        firstImageView = UIImageView()
-        firstImageView.translatesAutoresizingMaskIntoConstraints = false
-        firstImageView.tag = 1
         
     }
     
-    private func setUp(_ imageView: UIImageView) {
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-    }
 
 }
---
+++ TheNameGame/Views/ProfileCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileCollectionViewCell.swift
+//  TheNameGame
+//
+//  Created by Iyin Raphael on 3/18/21.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Properites
+    var profileImageView: UIImageView!
+    
+    var profile: Profile?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    
+        profileImageView = UIImageView()
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.contentMode = .scaleAspectFill
+        addSubview(profileImageView)
+        
+        NSLayoutConstraint.activate([
+            profileImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            profileImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            profileImageView.widthAnchor.constraint(equalTo: widthAnchor),
+            profileImageView.heightAnchor.constraint(equalTo: widthAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
